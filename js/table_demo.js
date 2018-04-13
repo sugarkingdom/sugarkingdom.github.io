da865afd8703c2af27ab84c152d53ce293fa42aa
sugarkingdom <sugarking1968@gmail.com>
2018-04-13 17:41:00 +0800

    fix

--- js/table_demo.js
+++ js/table_demo.js
@@ -1,3 +1,84 @@
+var tableData1 = [{
+	id: 1,
+	text1: '1_1 文本内容',
+	object1: {
+		key1: "1_2 对象内容"
+	},
+	input1: '1_3 输入框',
+	input2: '1_4 输入框',
+	select1: '1_5 选择框 01',
+	select1_options: '1_5 选择框 01#1_5 选择框 02#1_5 选择框 03',
+	select2: '1_6 选择框 03',
+	select2_options: ['1_6 选择框 01', '1_6 选择框 02', '1_6 选择框 03'],
+}, {
+	id: 2,
+	text1: '2_1 文本内容',
+	object1: {
+		key1: "2_2 对象内容"
+	},
+	input1: '2_3 输入框',
+	input2: '2_4 输入框',
+	select1: '2_5 选择框 02',
+	select1_options: '2_5 选择框 01#2_5 选择框 02#2_5 选择框 03',
+	select2: '2_6 选择框 02',
+	select2_options: ['2_6 选择框 01', '2_6 选择框 02', '2_6 选择框 03'],
+}, {
+	id: 3,
+	text1: '3_1 文本内容',
+	object1: {
+		key1: "3_2 对象内容"
+	},
+	input1: '3_3 输入框',
+	input2: '3_4 输入框',
+	select1: '3_5 选择框 03',
+	select1_options: '3_5 选择框 01#3_5 选择框 02#3_5 选择框 03',
+	select2: '3_6 选择框 01',
+	select2_options: ['3_6 选择框 01', '3_6 选择框 02', '3_6 选择框 03'],
+}];
+
+var tableData2 = [{
+	id: 1,
+	textarea1: '1_1 文本框内容 01\n1_1 文本框内容 02\n1_1 文本内容 03',
+	sex: 1,
+	headImgUrl: "01.png"
+}, {
+	id: 2,
+	textarea1: '2_1 文本框内容 01\n2_1 文本框内容 02\n2_1 文本内容 03',
+	sex: 2,
+	headImgUrl: "02.png"
+}, {
+	id: 3,
+	textarea1: '3_1 文本框内容 01\n3_1 文本框内容 02\n3_1 文本内容 03',
+	sex: 3,
+	headImgUrl: "03.png"
+}];
+
+var tableData3 = [{
+	id: 1,
+	money1: 1000,
+	money1Tag: "1",
+	money2: 3000,
+	money2Tag: "2",
+}, {
+	id: 2,
+	money1: 2000,
+	money1Tag: "1",
+	money2: 2000,
+	money2Tag: "3",
+}, {
+	id: 3,
+	money1: 3000,
+	money1Tag: "2",
+	money2: 1000,
+	money2Tag: "3",
+}, {
+	id: 3,
+	money1: 2000,
+	money1Tag: "2",
+	money2: 2000,
+	money2Tag: "3",
+}];
+
 var page1 = "1";
 var size1 = '10';
 
